58a9e581143288e73be6fcb28adb433dc8969fab
Sebastian Mandl <sebastian-mandl@gmx.net>
2018-03-30 17:50:13 +0200

    ---

--- plugins/meter.py
+++ plugins/meter.py
@@ -8,7 +8,7 @@ data = json.loads(input)
 
 os.system("sudo bash meter.bash")
 
-ser = serial.Serial('/dev/' + data["port"], 300)
+ser = serial.Serial('/dev/' + data["port"], baudrate=300, bytesize=7, parity=1,stopbits=1)
 
 data = ser.readline()
 while data:
