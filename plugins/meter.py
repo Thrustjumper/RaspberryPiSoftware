8e070269e5b22b151c53ec2d9651fb5e7bff9bf6
Sebastian Mandl <sebastian-mandl@gmx.net>
2018-03-30 15:23:17 +0200

    ---

--- plugins/meter.py
+++ plugins/meter.py
@@ -1,18 +1,14 @@
 import sys
 import serial
-
-#for line in sys.stdin:
-#	print(line);
-
-import subprocess
+import os
 import json
 
 input = sys.stdin.readline()
 data = json.loads(input)
 
-subprocess.call("meter.bash")
+os.system("sudo bash meter.bash")
 
-ser = serial.Serial('/dev/' + data["port"], 9600)
+ser = serial.Serial('/dev/' + data["port"], 300)
 
 data = ser.readline()
 while data:
